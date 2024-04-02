83d060fc3968cb93ade9a0fea782e714fc16baca
YuA83 <77770646+YuA83@users.noreply.github.com>
2024-04-03 00:15:51 +0900

    [level 0] Title: 조건 문자열, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,8 @@
+function solution(ineq, eq, n, m) {
+    switch(ineq+eq) {
+        case ">=": return +(n >= m);
+        case "<=": return +(n <= m);
+        case ">!": return +(n > m);
+        case "<!": return +(n < m);
+    }
+}
