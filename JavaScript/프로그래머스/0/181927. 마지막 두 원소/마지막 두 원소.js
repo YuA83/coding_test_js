e5e494ef87ebe3317f1a994124f52846f0288979
YuA83 <77770646+YuA83@users.noreply.github.com>
2024-04-03 10:40:21 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
+++ JavaScript/프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,7 +1,11 @@
 function solution(num_list) {
-    const x = num_list.reverse().reduce((p, c, i) => i === 1? (p > c? p-c : 2*p) : p);
+//     const x = num_list.reverse().reduce((p, c, i) => i === 1? (p > c? p-c : 2*p) : p);
     
-    num_list.reverse().push(x);
+//     num_list.reverse().push(x);
     
-    return num_list;
+//     return num_list;
+    
+    const [x, y] = [...num_list].reverse();
+    
+    return [...num_list, x > y? x-y : 2*x];
 }
