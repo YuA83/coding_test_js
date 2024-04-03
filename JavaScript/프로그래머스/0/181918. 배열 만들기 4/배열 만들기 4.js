c5e64be428c0611cd90b87e6ca237a8d22712421
YuA83 <77770646+YuA83@users.noreply.github.com>
2024-04-03 18:14:24 +0900

    [level 0] Title: 배열 만들기 4, Time: 4.37 ms, Memory: 37.9 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
+++ JavaScript/프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
@@ -1,19 +1,28 @@
 function solution(arr) {
     let stk = [];
-    let i = 0;
+//     let i = 0;
     
-    while (i < arr.length) {
-        if (!stk.length) {
-            stk.push(arr[i]);
-            i += 1;
-        }
-        if (stk.length && stk[stk.length - 1] < arr[i]) {
-            stk.push(arr[i]);
-            i += 1;
-        }
-        if (stk.length && stk[stk.length - 1] >= arr[i]) {
+//     while (i < arr.length) {
+//         if (!stk.length) {
+//             stk.push(arr[i]);
+//             i += 1;
+//         }
+//         if (stk.length && stk[stk.length - 1] < arr[i]) {
+//             stk.push(arr[i]);
+//             i += 1;
+//         }
+//         if (stk.length && stk[stk.length - 1] >= arr[i]) {
+//             stk.pop();
+//             continue;
+//         }
+//     }
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (!stk.length) stk.push(arr[i]);
+        else if (stk.slice(-1)[0] < arr[i]) stk.push(arr[i]);
+        else {
             stk.pop();
-            continue;
+            i--;
         }
     }
     
