8bd0993388258ea6c4b9bce0998356e8a00ee003
YuA83 <77770646+YuA83@users.noreply.github.com>
2024-04-04 12:26:05 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.03 ms, Memory: 33.7 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
+++ JavaScript/프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -1,3 +1,4 @@
 function solution(my_string, is_suffix) {
-    return +([...my_string].map((_, idx) => my_string.substr(idx)).includes(is_suffix));
+    // return +([...my_string].map((_, idx) => my_string.substr(idx)).includes(is_suffix));
+    return +(my_string.endsWith(is_suffix));
 }
