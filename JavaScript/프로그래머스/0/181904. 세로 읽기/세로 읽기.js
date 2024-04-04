fbbb710b46dc80821d00793442af00afe4f5f573
YuA83 <77770646+YuA83@users.noreply.github.com>
2024-04-04 14:42:33 +0900

    [level 0] Title: 세로 읽기, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181904. 세로 읽기/세로 읽기.js
+++ JavaScript/프로그래머스/0/181904. 세로 읽기/세로 읽기.js
@@ -1,10 +1,12 @@
 function solution(my_string, m, c) {
-    let arr = [];   
+//     let arr = [];   
     
-    for (let i = 0; i <= my_string.length/m; i++) {
-        arr.push(my_string.substr(i*m, m));
-    }
+//     for (let i = 0; i <= my_string.length/m; i++) {
+//         arr.push(my_string.substr(i*m, m));
+//     }
     
     
-    return arr.map(v => v.substr(c-1, 1)).join("");
+//     return arr.map(v => v.substr(c-1, 1)).join("");
+    
+    return [...my_string].filter((_, i) => i % m === c-1).join("");
 }
