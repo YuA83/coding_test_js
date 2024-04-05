5fb624ca1a71b3701f7dc2ae1213fbd799cecbde
YuA <77770646+YuA83@users.noreply.github.com>
2024-04-05 14:59:40 +0900

    [level 0] Title: 1로 만들기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181880. 1로 만들기/1로 만들기.js
+++ JavaScript/프로그래머스/0/181880. 1로 만들기/1로 만들기.js
@@ -1,4 +1,5 @@
 function solution(num_list) {
+    /*    
     return num_list.reduce((p, c) => {
         let x = c;
         
@@ -9,4 +10,15 @@ function solution(num_list) {
         
         return p;
     }, 0);
+    */
+    
+    /*
+    12  => 1100 => 4 => 3
+    4   => 0100 => 3 => 2
+    15  => 1111 => 4 => 3
+    1   => 0001 => 1 => 0
+    14  => 1110 => 4 => 3
+    */
+    
+    return num_list.reduce((p, c) => p + c.toString(2).length -1, 0);
 }
