ab6fcd942715de252d84de41648acdecbd82d9e5
YuA <77770646+YuA83@users.noreply.github.com>
2024-04-05 18:35:59 +0900

    [level 0] Title: 배열 만들기 6, Time: 26.09 ms, Memory: 38.2 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.js
+++ JavaScript/프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.js
@@ -1,10 +1,15 @@
 function solution(arr) {
     
     const stk = arr.reduce((p, c, i) => {
+        /*
         if (!p.length) return [...p, c];
         else if (p.slice(-1)[0] === c) return p.slice(0, -1);
         else return [...p, c];
+        */
+        
+        return p[p.length-1] !== c? [...p, c] : p.slice(0, -1);
     }, []);
+    
     
     return stk.length? stk : [-1];
 }
