543b156c0e9d8e9f44c513ca2de4466834d31f91
YuA <77770646+YuA83@users.noreply.github.com>
2024-04-05 17:04:02 +0900

    [level 0] Title: 세 개의 구분자, Time: 9.60 ms, Memory: 52.8 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
+++ JavaScript/프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
@@ -1,7 +1,9 @@
 function solution(myStr) {
-    const arr = myStr.replaceAll("a", " ").replaceAll("b", " ").replaceAll("c", " ")
-        .split(" ")
-        .filter(v => v);
+    // const arr = myStr.replaceAll("a", " ").replaceAll("b", " ").replaceAll("c", " ")
+    //     .split(" ")
+    //     .filter(v => v);
+
+    // return arr.length? arr : ["EMPTY"];
     
-    return arr.length? arr : ["EMPTY"];
+    return myStr.match(/[^a-c]+/g) || ["EMPTY"];
 }
