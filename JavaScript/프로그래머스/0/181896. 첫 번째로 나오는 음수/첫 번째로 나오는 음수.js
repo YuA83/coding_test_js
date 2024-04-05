0153e113fafa47e523d9e3529dcf1d069cf62e54
YuA83 <77770646+YuA83@users.noreply.github.com>
2024-04-05 10:41:35 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
+++ JavaScript/프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -1,4 +1,6 @@
 function solution(num_list) {
-    const ne = num_list.filter(v => v < 0);
-    return num_list.indexOf(ne[0]);
+    // const ne = num_list.filter(v => v < 0);
+    // return num_list.indexOf(ne[0]);
+    
+    return num_list.findIndex(v => v < 0);
 }
