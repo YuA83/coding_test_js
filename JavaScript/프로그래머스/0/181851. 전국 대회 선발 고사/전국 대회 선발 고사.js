55249d3accb35fe187fd650d5770e4c9f24d7db6
YuA <77770646+YuA83@users.noreply.github.com>
2024-04-09 17:29:44 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.18 ms, Memory: 33.7 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
+++ JavaScript/프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
@@ -1,10 +1,18 @@
 function solution(rank, attendance) {
+    /*
     let arr = [];
     
     attendance.forEach((v, i) => v? arr.push([i, rank[i]]) : "");
     const [a, b, c] = arr.sort((a, b) => a[1]-b[1]);
     
     return 10000*a[0] + 100*b[0] + c[0];
+    */
     
+    const [[a], [b], [c]] = rank
+    .map((v, i) => [i, v])
+    .filter((_, i) => attendance[i])
+    .sort((a, b) => a[1]-b[1]);
+    
+    return 10000 * a + 100 * b + c;
     
 }
