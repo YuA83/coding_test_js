8160444538cfda12eb5613bdb1d913cf3a0547e0
YuA <77770646+YuA83@users.noreply.github.com>
2024-04-09 18:13:11 +0900

    [level 0] Title: 0 떼기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181847. 0 떼기/0 떼기.js
+++ JavaScript/프로그래머스/0/181847. 0 떼기/0 떼기.js
@@ -1,11 +1,13 @@
 function solution(n_str) {
-    let arr = [...n_str];
+//     let arr = [...n_str];
     
-    for (const v of n_str) {
-        if (v !== "0") break;
+//     for (const v of n_str) {
+//         if (v !== "0") break;
         
-        arr.shift();
-    }
+//         arr.shift();
+//     }
     
-    return arr.join("");
+//     return arr.join("");
+    
+    return String(+n_str);
 }
