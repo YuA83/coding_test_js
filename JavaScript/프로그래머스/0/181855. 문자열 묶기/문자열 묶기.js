0e14325151df11378fa2575816c21f1d5c3c8c0f
YuA <77770646+YuA83@users.noreply.github.com>
2024-04-09 10:44:17 +0900

    [level 0] Title: 문자열 묶기, Time: 2.66 ms, Memory: 52.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/181855. 문자열 묶기/문자열 묶기.js
@@ -0,0 +1,13 @@
+function solution(strArr) {    
+    const len = strArr.reduce((p, c) => {
+        p[c.length] = (p[c.length] || 0) + 1;
+        return p;
+    }, {});
+    
+    const maxKey = Object.keys(len).reduce((p, c) => {
+        if (len[p] === len[c]) return +p > +c? p : c;
+        else return len[p] > len[c]? p : c;
+    });
+    
+    return len[maxKey];
+}
