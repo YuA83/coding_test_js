efacb9eeee9f0b83ab66b1bcc94d5748efc1336c
YuA <77770646+YuA83@users.noreply.github.com>
2024-04-09 18:12:18 +0900

    [level 0] Title: 0 떼기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/181847. 0 떼기/0 떼기.js
@@ -0,0 +1,11 @@
+function solution(n_str) {
+    let arr = [...n_str];
+    
+    for (const v of n_str) {
+        if (v !== "0") break;
+        
+        arr.shift();
+    }
+    
+    return arr.join("");
+}
