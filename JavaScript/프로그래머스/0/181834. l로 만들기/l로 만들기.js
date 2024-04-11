a0cda5145f9bb1a7ff335cc8ea46656559da2d60
YuA <77770646+YuA83@users.noreply.github.com>
2024-04-11 15:48:44 +0900

    [level 0] Title: l로 만들기, Time: 6.70 ms, Memory: 38.1 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181834. l로 만들기/l로 만들기.js
+++ JavaScript/프로그래머스/0/181834. l로 만들기/l로 만들기.js
@@ -1,3 +1,4 @@
 function solution(myString) {
-    return [...myString].map(v => v.charCodeAt() <= "l".charCodeAt()? "l" : v).join("");
+    // return [...myString].map(v => v.charCodeAt() <= "l".charCodeAt()? "l" : v).join("");
+    return [...myString].map(v => v < "l"? "l" : v).join("");
 }
