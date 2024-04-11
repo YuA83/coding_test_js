42db8dc113b57796ae173dcadebc5ecf067af9f1
YuA <77770646+YuA83@users.noreply.github.com>
2024-04-11 15:23:31 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.12 ms, Memory: 33.9 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
+++ JavaScript/프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
@@ -1,3 +1,4 @@
 function solution(date1, date2) {
-    return +(date1.map(v => v + "").join("")) < +(date2.map(v => v + "").join(""))? 1 : 0;
+    // return +(date1.map(v => v + "").join("")) < +(date2.map(v => v + "").join(""))? 1 : 0;
+    return new Date(date1) < new Date(date2)? 1: 0;
 }
