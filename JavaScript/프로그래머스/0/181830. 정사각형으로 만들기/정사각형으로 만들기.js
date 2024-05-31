35fbaa8e429ddea504956b474f63d73ff685e902
YuA <77770646+YuA83@users.noreply.github.com>
2024-05-31 16:58:40 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 0.06 ms, Memory: 38.3 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.js
+++ JavaScript/프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.js
@@ -1,17 +1,18 @@
 function solution(arr) {
-    let result;
-    const diff = Math.abs(arr.length - arr[0].length);
+    let result = arr;
+    const rLen = arr.length;
+    const cLen = arr[0].length;
+    const diff = Math.abs(rLen - cLen);
     
-    if (arr.length > arr[0].length) {
+    if (rLen > cLen) {
         result = arr.map(v => {
             v.push(Array(diff).fill(0));
             return v.flat();
         });
     } else {
-        result = arr;
-        
+       
         for (let i = 0; i < diff; i++) {
-            result.push(Array(result[0].length).fill(0));
+            result.push(Array(cLen).fill(0));
         }
     }
     
