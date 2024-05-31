bfd9b36aa8397dd8401e472ea5908a87589a4004
YuA <77770646+YuA83@users.noreply.github.com>
2024-05-31 17:35:50 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
+++ JavaScript/프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
@@ -1,11 +1,13 @@
 function solution(arr) {
-    let answer = true;
+    // let answer = true;
     
     for (let i = 0; i < arr.length; i++) {
         for (let j = i; j < arr.length; j++) {
-            answer &&= (arr[i][j] === arr[j][i]);
+            // answer &&= (arr[i][j] === arr[j][i]);
+            if (arr[i][j] !== arr[j][i]) return 0;
         }
     }
     
-    return +answer;
+    // return +answer;
+    return 1;
 }
