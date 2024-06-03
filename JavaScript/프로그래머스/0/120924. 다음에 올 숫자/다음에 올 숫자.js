b653e98025650f5d05df0b41081519cc818db4fb
YuA <77770646+YuA83@users.noreply.github.com>
2024-06-03 10:36:07 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.js
+++ JavaScript/프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -1,6 +1,7 @@
 function solution(common) {
-    const len = common.length -1;
+    // const len = common.length -1;
     const [x, y, z] = common;
     
-    return (y-x) === (z-y)? common[len] + (y-x) : common[len] * (y/x)
+    // return (y-x) === (z-y)? common[len] + (y-x) : common[len] * (y/x);
+    return (y-x) === (z-y)? common.pop() + (y-x) : common.pop() * (y/x)
 }
