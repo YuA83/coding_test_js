4ae42dbf07496437d815a78169daae0a9110f7d0
YuA <77770646+YuA83@users.noreply.github.com>
2024-06-12 12:40:33 +0900

    [level 0] Title: 문자열 밀기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
@@ -0,0 +1,13 @@
+function solution(A, B) {
+    let a = [...A];
+    
+    for (let i = 0; i < a.length; i++) {
+        a.unshift(a.pop());
+        
+        if (a.join("") === B) {
+            return i === a.length-1? 0 : i+1;
+        }
+    }
+    
+    return -1;
+}
